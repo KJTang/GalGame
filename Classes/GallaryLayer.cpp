e9eda1748f533465415198ab8b957495df114f89
KJTang <kaijietang@outlook.com>
2015-12-09 16:53:42 +0800

    feature: finished the basic features of "GallaryLayer"

--- Classes/GallaryLayer.cpp
+++ Classes/GallaryLayer.cpp
@@ -11,6 +11,8 @@
 #include <fstream>
 #include <string>
 
+#include "GyroBackground.h"
+
 /***************
  GallaryList
  ***************/
@@ -23,43 +25,128 @@ bool GallaryList::init()
         return false;
     }
     visibleSize = Director::getInstance()->getVisibleSize();
+    thumbWidth = visibleSize.width/4;
+    thumbSpace = visibleSize.width/3.5;
+    currentThumb = 0;
+    isClick = false;
     
-    auto test = Sprite::create("HelloWorld.png");
-    this->addChild(test);
-    test->setScale(1, visibleSize.height/test->getContentSize().height*0.3);
-    
-    GallaryLayer::setGallaryState(5, true);
-    GallaryLayer::setGallaryState(9, true);
-    
-    log("get %d", GallaryLayer::getGallaryState(5));
-    log("get %d", GallaryLayer::getGallaryState(6));
-    
-    touchListener = EventListenerTouchOneByOne::create();
-    touchListener->setSwallowTouches(false);
-    touchListener->onTouchBegan = [&, test](Touch *touch, Event *event) {
-        startPoint = touch->getLocation();
-        // relative position
-        auto locationInNode = test->convertToNodeSpace(touch->getLocation());
-        auto rect = cocos2d::Rect(0, 0, test->getContentSize().width, test->getContentSize().height);
-        if (rect.containsPoint(locationInNode)) {
-            touchListener->setSwallowTouches(true);
-            return true;
+//    GallaryLayer::setGallaryState(0, true);
+//    GallaryLayer::setGallaryState(1, true);
+//    GallaryLayer::setGallaryState(3, true);
+//    GallaryLayer::setGallaryState(4, true);
+//    GallaryLayer::setGallaryState(5, true);
+//    GallaryLayer::setGallaryState(9, true);
+
+    for (int i = 0; i != picCount; ++i) {
+        char name[30];
+        sprintf(name, "gallary/gallary%d.png", i);
+        
+        auto listItem = Sprite::create(name);
+        this->addChild(listItem);
+        thumbs.push_back(listItem);
+        listItem->setScale(thumbWidth/listItem->getContentSize().width, visibleSize.height/listItem->getContentSize().height/4);
+        listItem->setPosition(Vec2(visibleSize.width/2+thumbSpace*i, 0));
+        if (!GallaryLayer::getGallaryState(i)) {
+            listItem->setOpacity(100);
         }
-        return false;
-    };
-    touchListener ->onTouchMoved = [&](Touch *touch, Event *event) {
-        auto currentPoint = touch->getLocation();
-        this->setPositionX(this->getPositionX() + currentPoint.x - startPoint.x);
-        startPoint = currentPoint;
-        return true;
-    };
-    touchListener->onTouchEnded = [&](Touch *touch, Event *event) {
-        touchListener->setSwallowTouches(false);
-        return true;
-    };
-    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+        
+        touchListener = EventListenerTouchOneByOne::create();
+        touchListener->setSwallowTouches(true);
+        touchListener->onTouchBegan = [&, listItem](Touch *touch, Event *event) {
+            isClick = true;
+            event->getCurrentTarget()->stopActionByTag(tag_loadPic);
+            startPoint = touch->getLocation();
+            movePoint = startPoint;
+            // relative position
+            auto locationInNode = listItem->convertToNodeSpace(touch->getLocation());
+            auto rect = cocos2d::Rect(0, 0, listItem->getContentSize().width, listItem->getContentSize().height);
+            if (rect.containsPoint(locationInNode)) {
+                touchListener->setSwallowTouches(true);
+                return true;
+            }
+            return false;
+        };
+        touchListener ->onTouchMoved = [&](Touch *touch, Event *event) {
+            auto currentPoint = touch->getLocation();
+            this->setPositionX(this->getPositionX() + currentPoint.x - movePoint.x);
+            if (currentPoint.x - movePoint.x > 50 || currentPoint.x - movePoint.x < 100) {
+                isClick = false;
+            }
+            movePoint = currentPoint;
+            return true;
+        };
+        touchListener->onTouchEnded = [&](Touch *touch, Event *event) {
+            touchListener->setSwallowTouches(false);
+            auto target = static_cast<GallaryList*>(event->getCurrentTarget());
+            currentThumb = static_cast<int>((event->getCurrentTarget()->getPositionX()-thumbSpace/2)/thumbSpace);
+//            log("posx: %d", currentThumb);
+            if (-currentThumb >= picCount) {
+                currentThumb = -picCount + 1;
+            }
+            if (isClick) {
+                log("clicked: %d", currentThumb);
+                cocos2d::Point locationInNode = target->thumbs.at(-currentThumb)->convertToNodeSpace(touch->getLocation());
+                cocos2d::Rect rect = cocos2d::Rect(0, 0, target->thumbs.at(-currentThumb)->getContentSize().width, target->thumbs.at(-currentThumb)->getContentSize().height);
+                // open display mode
+                if (rect.containsPoint(locationInNode) && GallaryLayer::getGallaryState(-currentThumb)) {
+                    target->onPicClick(-currentThumb);
+                }
+            } else {
+                if (currentThumb >= 0) {
+                    target->runAction(MoveTo::create(0.2, Vec2(0, target->getPositionY())));
+                } else if (currentThumb < -picCount+1) {
+                    target->runAction(MoveTo::create(0.2, Vec2(thumbSpace*(-picCount+1), target->getPositionY())));
+                } else {
+                    target->runAction(MoveTo::create(0.2, Vec2(thumbSpace*currentThumb, target->getPositionY())));
+                }
+//                target->thumbs.at(-currentThumb)->runAction();
+                // change pic
+                target->onPicChange(-currentThumb);
+            }
+            return true;
+        };
+        _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+    }
     
     return true;
+}
+
+void GallaryList::onPicClick(int id) {
+    // TODO:
+    auto displayScene = Scene::create();
+    Director::getInstance()->pushScene(displayScene);
+    
+    char filename[30];
+    sprintf(filename, "gallary/gallary%d.png", id);
+    auto newPic = GyroBackground::create(filename, 1.5);
+    
+    displayScene->addChild(newPic);
+    newPic->setOpacity(0);
+    newPic->runAction(FadeIn::create(0.5));
+    
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->setSwallowTouches(true);
+    touchListener->onTouchBegan = [&](Touch *touch, Event *event) {
+        Director::getInstance()->popScene();
+        return false;
+    };
+    
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, displayScene);
+}
+
+void GallaryList::onPicChange(int id) {
+    char filename[30];
+    sprintf(filename, "gallary/gallary%d.png", id);
+    auto newPic = Sprite::create(filename);
+    newPic->retain();
+    auto loadPicAction = Sequence::create(DelayTime::create(0.5),
+                                          CallFunc::create([&, newPic]
+                                                           {
+                                                               static_cast<GallaryContent*>(this->getParent())->setDisplayPic(newPic);
+                                                           }),
+                                          NULL);
+    loadPicAction->setTag(tag_loadPic);
+    this->runAction(loadPicAction);
 }
 
 /***************
@@ -75,16 +162,28 @@ bool GallaryContent::init()
         return false;
     }
     visibleSize = Director::getInstance()->getVisibleSize();
+    currentPic = nullptr;
     
-    auto pic = Sprite::create("HelloWorld.png");
-    this->addChild(pic);
-    pic->setPosition(visibleSize.width*0.5, visibleSize.height*1.8);
+    currentPic = Sprite::create("HelloWorld.png");
+    this->addChild(currentPic);
+    currentPic->setPosition(visibleSize.width*0.5, visibleSize.height*1.7);
     
     auto list = GallaryList::create();
     this->addChild(list);
-    list->setPosition(visibleSize.width/2, visibleSize.height*1.3);
+    list->setPosition(0, visibleSize.height*1.2);
     
     return true;
+}
+
+void GallaryContent::setDisplayPic(Sprite *pic) {
+    if (currentPic) {
+        currentPic->removeFromParent();
+    }
+    currentPic = pic;
+    this->addChild(currentPic);
+    currentPic->setPosition(visibleSize.width*0.5, visibleSize.height*1.7);
+    currentPic->setOpacity(0);
+    currentPic->runAction(FadeIn::create(0.5));
 }
 
 /**************
@@ -153,7 +252,6 @@ bool GallaryLayer::init()
 void GallaryLayer::setGallaryState(int id, bool state)
 {
     std::string path = FileUtils::getInstance()->getWritablePath()+"Gallary.txt";
-    int picCount = 10; // TODO: gallary pic count, as a temp var here
     if (!FileUtils::getInstance()->isFileExist(path)) {
         log("file not exist");
         std::ofstream fout(path);
@@ -179,7 +277,6 @@ void GallaryLayer::setGallaryState(int id, bool state)
 bool GallaryLayer::getGallaryState(int id)
 {
     std::string path = FileUtils::getInstance()->getWritablePath()+"Gallary.txt";
-    int picCount = 10; // TODO: gallary pic count, as a temp var here
     if (!FileUtils::getInstance()->isFileExist(path)) {
         log("file not exist");
         std::ofstream fout(path);
--- Classes/GallaryLayer.h
+++ Classes/GallaryLayer.h
@@ -12,20 +12,34 @@
 #include "cocos2d.h"
 #include <vector>
 
+const int picCount = 10; // gallary pic count, as a temp var here
+const int tag_loadPic = 0;
+
 using namespace cocos2d;
 
 class GallaryList : public Layer
 {
 private:
     Size visibleSize;
+    float thumbWidth;
+    float thumbSpace;
+    int currentThumb;
+public:
+    std::vector<Sprite*> thumbs;
+private:
     EventListenerTouchOneByOne *touchListener;
-    Point startPoint, endPoint;
+    Point startPoint, movePoint, endPoint;
+    bool isClick;
     std::vector<bool> picAvailable;
 public:
     GallaryList();
     ~GallaryList();
     virtual bool init();
     CREATE_FUNC(GallaryList);
+    
+    // callback
+    void onPicClick(int id);
+    void onPicChange(int id);
 };
 
 class GallaryContent : public Layer
@@ -33,11 +47,15 @@ class GallaryContent : public Layer
 private:
     Size visibleSize;
     EventListenerTouchOneByOne *touchListener;
+    Sprite *currentPic;
 public:
     GallaryContent();
     ~GallaryContent();
     virtual bool init();
     CREATE_FUNC(GallaryContent);
+    
+    // set displaying pic
+    void setDisplayPic(Sprite *pic);
 };
 
 class GallaryLayer : public Layer
